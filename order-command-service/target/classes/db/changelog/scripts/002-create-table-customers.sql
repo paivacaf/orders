90d0bbb125197cd0a950d3e02c9d36961d30b6fa
Charles Paiva <paiva.charles@gmail.com>
2024-04-26 02:38:39 -0300

    Commit inicial

--- order-command-service/target/classes/db/changelog/scripts/002-create-table-customers.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `customers` (
-    `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `client_code` INT NOT NULL UNIQUE,
-    `name` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
